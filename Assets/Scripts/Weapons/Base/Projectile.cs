f2df1885a2bf3c7493f85d6b27f03c84534b93ea
Powertr1p <sc2aru@gmail.com>
2023-02-22 16:36:02 +0400

    added logic to play hit animation

--- Assets/Scripts/Weapons/Base/Projectile.cs
+++ Assets/Scripts/Weapons/Base/Projectile.cs
@@ -5,6 +5,8 @@ namespace PowerTrip
     public class Projectile : MonoBehaviour, IDamaging
     {
         [SerializeField] private Transform _visual;
+        [SerializeField] private bool _hasHitEffect = false;
+        [SerializeField] private GameObject _deathEffect;
         
         private Vector3 _direction;
         private float _speed;
@@ -12,8 +14,10 @@ namespace PowerTrip
 
         private int _maxNumberOfCollisions = 1;
         private int _currentNumberOfCollisions;
+        private float _destroyTimer = 0;
 
         private bool _isInitialized;
+        
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -57,7 +61,10 @@ namespace PowerTrip
             if (_currentNumberOfCollisions >= _maxNumberOfCollisions)
             {
                 // TODO: use pooling
-                Destroy(gameObject);
+                PlayHitEffect();
+                Destroy(gameObject, _destroyTimer);
+                
+                _isInitialized = false;
             }
         }
 
@@ -68,6 +75,15 @@ namespace PowerTrip
                 float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
                 _visual.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
             }
+        }
+
+        private void PlayHitEffect()
+        {
+            if (!_hasHitEffect) return;
+            
+            _visual.gameObject.SetActive(false);
+            _deathEffect.SetActive(true);
+            _destroyTimer = _deathEffect.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length;
         }
     }
 }
