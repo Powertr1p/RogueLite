c2f637bfe2416b8a867015131e1a46747642bb00
Powertr1p <sc2aru@gmail.com>
2023-02-27 18:51:16 +0400

    added new fonts and logic [2]

--- Assets/Scripts/Blood/BloodAnimation.cs
+++ Assets/Scripts/Blood/BloodAnimation.cs
@@ -7,8 +7,6 @@ namespace DefaultNamespace
     {
         public void Start()
         {
-            transform.localScale = Vector3.zero;
-
             var euler = transform. eulerAngles;
             euler.z = Random. Range(0.0f, 360.0f);
             transform. eulerAngles = euler;
